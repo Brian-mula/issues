9485cf2edb1c5c2565868b1c6faf8f9481b13750
Brian-mula <mulatiwangilabrian@gmail.com>
2023-09-21 09:57:54 +0300

    added rounded circle

---
+++ src/components/rounded.tsx
@@ -0,0 +1,8 @@
+
+export default function Rounded() {
+  return (
+    <div className='w-4 h-4 rounded-full border border-green-600 flex items-center justify-center content-center mx-2'>
+        <div className='w-1 h-1 rounded-full bg-green-600'></div>
+    </div>
+  )
+}
