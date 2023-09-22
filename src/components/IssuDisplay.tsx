e12b0e43beb5333f87aabeba239a3e357efd8256
Brian-mula <mulatiwangilabrian@gmail.com>
2023-09-22 10:39:03 +0300

    added issue display

--- src/components/IssuDisplay.tsx
+++ src/components/IssuDisplay.tsx
@@ -1,23 +1,49 @@
 import { GitHubIssueType } from "@/types/issueType";
+import Image from "next/image";
+import Link from "next/link";
 import Rounded from "./rounded";
 
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "April",
+  "May",
+  "June",
+  "July ",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+
 export default function IssuDisplay({ issue }: { issue: GitHubIssueType }) {
+  const date = new Date(issue.created_at);
+  const month = date.getHours();
+  const day = date.getDay();
+
   return (
-    <div className="bg-gray-500 px-3 py-4 flex items-center justify-between border border-t border-gray-300">
+    <div  className="bg-gray-700 px-3 py-4 flex items-center justify-between border border-t border-gray-300 hover:bg-gray-600">
       <div className="flex items-start">
         <input type="checkbox" />
         <Rounded />
         <div>
-          <h1>{issue.title}</h1>
+
+          <Link href={`/issues/${encodeURIComponent(issue.number)}`} className="hover:text-blue-500 text-lg">{issue.title}</Link>
           <p className="text-xs">
-            #{issue.number} Opened on {issue.title.slice(0, 6)} By{" "}
+            #{issue.number} Opened on {day} {months[month]} By{" "}
             {issue.user.login}
           </p>
         </div>
       </div>
       <div className="flex items-center">
         {
-            issue.assignee && <img src={issue.assignee.avater_url} alt={issue.assignee.login} className="h-12 w-12 rounded-full" />
+            issue.assignee &&  <div className="tooltip" data-tip={`Assigned to ${issue.assignee.login}`} >
+              <Image src={issue.assignee.avatar_url} alt="Picture of the author" width={30} height={30} className="rounded-full" />
+            </div>
+            
         }
         <div></div>
       </div>
