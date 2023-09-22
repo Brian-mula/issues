1837f3056c3f4c3295c4d31c0b808aa4c9a1ae35
Brian-mula <mulatiwangilabrian@gmail.com>
2023-09-22 10:38:38 +0300

    added dropdown

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,23 +1,75 @@
 
-export default function DropDown() {
+
+import { MemberType } from "@/types/memberTypes";
+import { Octokit } from "@octokit/core";
+import { useEffect, useReducer } from "react";
+
+const octokit = new Octokit({
+  auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
+});
+
+const initialState = {
+  members: [] as MemberType[],
+  status: "loading",
+};
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "isready":
+      return {
+        ...state,
+        members: action.payload,
+        status: "ready",
+      };
+    case "onError":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+export default function DropDown({ title,onFilter }: { title: string,onFilter:Function }) {
+  const [{ members, status }, dispatch] = useReducer(reducer, initialState);
+
+  // const handleMemberIssues = (login:string) => {
+  //    issues.filter((issue:GitHubIssueType) => issue.assignee?.login === login)
+    
+  // }
+
+  useEffect(() => {
+    octokit
+      .request("GET /orgs/{org}/members", {
+        org: "Taquana-LTD",
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      })
+      .then((res) => dispatch({ type: "isready", payload: res.data }))
+      .catch((err) => dispatch({ type: "onError" }));
+  },[]);
+  console.log(members)
   return (
     <div>
-      <div className="dropdown dropdown-end">
-        <label tabIndex={0} className="btn m-1">
-          Click
-        </label>
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <a>Item 2</a>
-          </li>
-        </ul>
-      </div>
+      {status === "ready" && (
+        <div className="dropdown dropdown-bottom dropdown-end">
+          <label tabIndex={0} className="m-1 cursor-pointer">{title}</label>
+          <ul tabIndex={0} className="p-2 shadow menu dropdown-content z-[1] bg-gray-600 rounded-sm w-64 h-96 overflow-y-auto block">
+            {
+              members.map((member:MemberType) => <li key={member.avatar_url} className="p-2 hover:bg-gray-500">
+                <div onClick={()=>onFilter(member.login)} className="flex items-center">
+                  <img src={member.avatar_url} alt="" className="rounded-full w-6 h-6"/>
+                  <p className="px-2">{member.login}</p>
+                </div>
+              </li>)
+            }
+            
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
