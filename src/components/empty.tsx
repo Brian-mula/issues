54f93689060aa75a1a8b0e48d591437a895d2268
Brian-mula <mulatiwangilabrian@gmail.com>
2023-09-22 10:38:52 +0300

    empty component

---
+++ src/components/empty.tsx
@@ -0,0 +1,8 @@
+
+export default function NoItemsFound() {
+  return (
+    <div className='py-6 px-6 mt-auto mb-auto'>
+        <p className='text-center text-gray-400 text-xl'>No results matched your search</p>
+    </div>
+  )
+}
