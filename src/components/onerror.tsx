37677c5a4f3452ff3861c6d2b6376adb3e4eb886
Brian-mula <mulatiwangilabrian@gmail.com>
2023-09-22 10:39:36 +0300

    added error component

--- src/components/onerror.tsx
+++ src/components/onerror.tsx
@@ -1,8 +1,8 @@
 
 export default function OnError() {
   return (
-    <div>
-        <p>An Error occured</p>
+    <div className="py-3 px-2 rounded-full bg-gray-500">
+        <p className="text-center">Oops!! Something went wrong</p>
     </div>
   )
 }
