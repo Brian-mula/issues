2a3216807a418108f64df6d5778564a8d1c93d28
Brian-mula <mulatiwangilabrian@gmail.com>
2023-09-23 11:06:39 +0300

    modified navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,29 @@
+"use client"
 
-export default function navbar() {
+import { signOut, useSession } from "next-auth/react"
+
+export default function Navbar() {
+    const {data,status}= useSession()
   return (
-    <div>navbar</div>
+    <>
+    {
+        status==='authenticated' &&
+        <div className="navbar bg-gray-600">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">IBUQA ISSUES</a>
+        </div>
+        <div className="flex-none">
+          <div className="flex items-center">
+           <p className="px-3">
+              { data?.user?.name }
+           </p>
+           <button onClick={()=>signOut()} className="btn btn-warning btn-sm text-white font-bold">
+             Logout
+           </button>
+          </div>
+        </div>
+      </div>
+    }
+    </>
   )
 }
