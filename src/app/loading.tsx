98dc3cad9a26ea68b71ec26d679ba58229f1ac9a
Brian-mula <mulatiwangilabrian@gmail.com>
2023-09-27 09:11:09 +0300

    added loading effect

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,8 @@
 
 export default function Loading() {
   return (
-    <div>
-        Loading...
+    <div className="h-screen pt-20 ml-auto mr-auto">
+        <p className="text-center text-3xl">Loading...</p>
     </div>
   )
 }
