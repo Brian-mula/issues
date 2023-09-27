07676096c0f439d1b8364226fb5f4bc3931d4ccb
Brian-mula <mulatiwangilabrian@gmail.com>
2023-09-27 09:14:18 +0300

    modified loader

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,6 +1,10 @@
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function Loader() {
   return (
-    <div>loadeing ...</div>
+    <div className="min-h-screen w-full pt-20 mr-auto ml-auto">
+     <Skeleton count={6} />
+    </div>
   )
 }
